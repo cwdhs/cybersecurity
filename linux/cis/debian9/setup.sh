27b7f5848465d4798bc88b679790057fd1481adb
matteopolak <matteopolak@hotmail.com>
2021-12-05 20:46:54 -0500

    add debian 9

--- linux/cis/debian9/setup.sh
+++ linux/cis/debian9/setup.sh
@@ -18,32 +18,6 @@ cat > /etc/ansible/harden.yml << EOF
   hosts: localhost
   connection: local
   become: yes
-	debian9cis_avahi_server: false  
-	debian9cis_cups_server: false  
-	debian9cis_dhcp_server: false  
-	debian9cis_ldap_server: false  
-	debian9cis_telnet_server: false  
-	debian9cis_nfs_server: false  
-	debian9cis_rpc_server: false  
-	debian9cis_ntalk_server: false  
-	debian9cis_rsyncd_server: false  
-	debian9cis_tftp_server: false  
-	debian9cis_rsh_server: false  
-	debian9cis_nis_server: false  
-	debian9cis_snmp_server: false  
-	debian9cis_squid_server: false  
-	debian9cis_smb_server: false  
-	debian9cis_dovecot_server: false  
-	debian9cis_httpd_server: false  
-	debian9cis_vsftpd_server: false  
-	debian9cis_named_server: false  
-	debian9cis_bind: false  
-	debian9cis_vsftpd: false  
-	debian9cis_httpd: false  
-	debian9cis_dovecot: false  
-	debian9cis_samba: false  
-	debian9cis_squid: false  
-	debian9cis_net_snmp: false
 
   roles:
     - Debian9-CIS
