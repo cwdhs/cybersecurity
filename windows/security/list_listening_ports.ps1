f4c322dbd5d2daf2cd522372fd41a9e15d32b047
matteopolak <matteopolak@hotmail.com>
2021-12-08 09:32:14 -0500

    netstat command

---
+++ windows/security/list_listening_ports.ps1
@@ -0,0 +1,6 @@
+# prints out network connections and open ports
+# b - displays name of executable responsible for entry
+# f - displays FQDN for foreign addresses
+# n - displays addresses and port # as numbers
+# o - shows process ID of owner
+netstat -bfno
